1e9a37a49014ac8cd4fab48e3efadda217d0e218
Liliyafamiliya <lilyalapetina@gmail.com>
2023-10-12 18:28:58 +0400

    feat: add Math.ceil and Math.floor

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,8 +1,37 @@
+// class GuessingGame {
+//   constructor() {
+//     this.min = 0;
+//     this.max = 0;
+//     this.guessNum = 0;
+//   }
+
+//   setRange(min, max) {
+//     this.min = min;
+//     this.max = max;
+//   }
+
+//   guess() {
+//     this.guessNum = Math.floor((this.min + this.max) / 2);
+//     return this.guessNum;
+//   }
+
+//   lower() {
+//     this.max = this.guessNum;
+//   }
+
+//   greater() {
+//     this.min = this.guessNum + 1;
+//   }
+// }
+
+// module.exports = GuessingGame;
+
 class GuessingGame {
   constructor() {
     this.min = 0;
     this.max = 0;
     this.guessNum = 0;
+    this.useCeil = false;
   }
 
   setRange(min, max) {
@@ -11,16 +40,22 @@ class GuessingGame {
   }
 
   guess() {
-    this.guessNum = Math.round((this.min + this.max) / 2);
+    if (this.useCeil) {
+      this.guessNum = Math.ceil((this.min + this.max) / 2);
+    } else {
+      this.guessNum = Math.floor((this.min + this.max) / 2);
+    }
     return this.guessNum;
   }
 
   lower() {
     this.max = this.guessNum;
+    this.useCeil = false;
   }
 
   greater() {
     this.min = this.guessNum + 1;
+    this.useCeil = true;
   }
 }
 
